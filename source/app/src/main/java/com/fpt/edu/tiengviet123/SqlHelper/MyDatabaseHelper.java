d57b5b0a21e35c84346c31eafc08bfc528960a62
pdoan4 <pdoan4@csc.com>
2018-07-04 17:00:52 +0700

    [ALS-0000] update helper mysql

--- source/app/src/main/java/com/fpt/edu/tiengviet123/SqlHelper/MyDatabaseHelper.java
+++ source/app/src/main/java/com/fpt/edu/tiengviet123/SqlHelper/MyDatabaseHelper.java
@@ -7,23 +7,27 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import com.fpt.edu.sqlitedemo.bean.Note;
 
-import java.lang.annotation.Target;
+import com.fpt.edu.tiengviet123.bean.VCharacter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "NoteDB";
+    private static final String DATABASE_NAME = "CharacterDB";
     private static final int DATABASE_VERSION = 1;
-    private static final String TABLE_NAME = "Notes";
-    private static final String COLUME_NOTE_ID = "_id";
-    private static final String COLUMN_NOTE_TITLE = "Note_Title";
-    private static final String COLUMN_NOTE_CONTENT = "Note_Content";
+    private static final String TABLE_NAME = "Characters";
+    private static final String COLUME_ID = "_id";
+    private static final String COLUMN_NAME = "Name";
+    private static final String COLUMN_CHARACTER_CONTENT = "Content";
+    private static final String COLUMN_IMAGE_SOURCE_ID = "ImageScrId";
+    private static final String COLUMN_BASE_AUDIO_SOURCE_ID = "BaseAudioScrId";
+    private static final String COLUMN_EXTENSION_AUDIO_SOURCE_ID = "ExtensionAudioScrId";
+
     final static String TAG = "MYSQLHEPLER";
 
-    private static final String SCRIPT_CREATE = "CREATE TABLE " + TABLE_NAME + "(" + COLUME_NOTE_ID + " INTEGER PRIMARY KEY," + COLUMN_NOTE_TITLE + " TEXT," + COLUMN_NOTE_CONTENT + " TEXT"+")";
+    private static final String SCRIPT_CREATE = "CREATE TABLE " + TABLE_NAME + "(" + COLUME_ID + " INTEGER PRIMARY KEY," + COLUMN_NAME + " TEXT," + COLUMN_CHARACTER_CONTENT + " TEXT," + COLUMN_IMAGE_SOURCE_ID + " INTEGER," + COLUMN_BASE_AUDIO_SOURCE_ID + " INTEGER, " + COLUMN_EXTENSION_AUDIO_SOURCE_ID + " INTEGER )";
 
     SQLiteDatabase database = this.getReadableDatabase();
 
@@ -34,7 +38,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         Log.i(TAG, "OnCreate Database");
-        Log.i(TAG,SCRIPT_CREATE);
+        Log.i(TAG, SCRIPT_CREATE);
         db.execSQL(SCRIPT_CREATE);
 
     }
@@ -48,10 +52,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public void insertDefaultNote() {
         if (this.getNodteCount() == 0) {
-            Note n1 = new Note("TEST1", "this is test for default create note");
-            Note n2 = new Note("TEST 2", "This is another test content for create default note");
-            addNote(n1);
-            addNote(n2);
+
         }
 
     }
@@ -59,64 +60,70 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public int getNodteCount() {
         Log.i(TAG, "Get note count");
 
-        String query = "SELECT * FROM Notes";
+        String query = "SELECT * FROM " + TABLE_NAME;
+
         Cursor cursor = database.rawQuery(query, null);
         return cursor.getCount();
     }
 
-    public void addNote(Note note) {
+    public void addNote(VCharacter node) {
         Log.i(TAG, "Adding  a note");
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COLUMN_NOTE_CONTENT, note.getContent());
-        contentValues.put(COLUMN_NOTE_TITLE, note.getTitle());
+        contentValues.put(COLUMN_NAME, node.getFace());
+        contentValues.put(COLUMN_IMAGE_SOURCE_ID, node.getContent());
+        contentValues.put(COLUMN_IMAGE_SOURCE_ID, node.getImageSrcId());
+        contentValues.put(COLUMN_BASE_AUDIO_SOURCE_ID, node.getBaseAudioId());
+        contentValues.put(COLUMN_EXTENSION_AUDIO_SOURCE_ID, node.getExtentionAudioID());
         database.insert(TABLE_NAME, null, contentValues);
     }
 
-    public Note getNote(int id) {
-        Log.i(TAG, "GET note ");
-        Cursor cursor = database.query(TABLE_NAME, new String[]{COLUME_NOTE_ID, COLUMN_NOTE_TITLE, COLUMN_NOTE_CONTENT}, COLUME_NOTE_ID + "=?", new String[]{String.valueOf(id)}, null, null, null, null);
+    public VCharacter getVCharacter(String name) {
+        Log.i(TAG, "GET vChar ");
+        Cursor cursor = database.query(TABLE_NAME, new String[]{COLUME_ID, COLUMN_NAME, COLUMN_CHARACTER_CONTENT, COLUMN_IMAGE_SOURCE_ID, COLUMN_BASE_AUDIO_SOURCE_ID, COLUMN_EXTENSION_AUDIO_SOURCE_ID}, COLUMN_NAME + "=?", new String[]{String.valueOf(name)}, null, null, null, null);
         if (cursor != null) {
             cursor.moveToFirst();
-            Note note = new Note(cursor.getInt(0), cursor.getString(1), cursor.getString(2));
-            return note;
+            VCharacter node = new VCharacter(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getInt(3), cursor.getInt(4), cursor.getInt(5));
+            return node;
         }
         return null;
     }
 
-    public List<Note> getAllNote() {
+    public List<VCharacter> getAllNote() {
 
         String query = "SELECT * FROM " + TABLE_NAME;
         Cursor cursor = database.rawQuery(query, null);
-        ArrayList<Note> list = new ArrayList<>();
+        ArrayList<VCharacter> list = new ArrayList<>();
         while (cursor.moveToNext()) {
-
-            Note note = new Note(cursor.getInt(0), cursor.getString(1), cursor.getString(2));
-            list.add(note);
+            VCharacter node = new VCharacter(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getInt(3), cursor.getInt(4), cursor.getInt(5));
+            list.add(node);
         }
         return list;
     }
 
-    public boolean update(Note note) {
-        Log.i(TAG, "MyDatabaseHelper.updateNote ... " + note.getTitle());
+    public boolean update(VCharacter node) {
+        Log.i(TAG, "MyDatabaseHelper. Update Character ... " + node.getFace());
 
         SQLiteDatabase db = this.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_NOTE_TITLE, note.getTitle());
-        values.put(COLUMN_NOTE_CONTENT, note.getContent());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME, node.getFace());
+        contentValues.put(COLUMN_IMAGE_SOURCE_ID, node.getContent());
+        contentValues.put(COLUMN_IMAGE_SOURCE_ID, node.getImageSrcId());
+        contentValues.put(COLUMN_BASE_AUDIO_SOURCE_ID, node.getBaseAudioId());
+        contentValues.put(COLUMN_EXTENSION_AUDIO_SOURCE_ID, node.getExtentionAudioID());
 
         // updating row
-        return db.update(TABLE_NAME, values, COLUME_NOTE_ID + " = ?",
-                new String[]{String.valueOf(note.getNoteId())})>0;
-
+        return db.update(TABLE_NAME, contentValues, COLUME_ID + " = ?",
+                new String[]{String.valueOf(node.getId())})>0;
 
     }
-    public void deleteNote(Note note) {
-        Log.i(TAG, "MyDatabaseHelper.updateNote ... " + note.getTitle() );
+
+    public void deleteNote(VCharacter note) {
+        Log.i(TAG, "MyDatabaseHelper.updateNote ... " + note.getFace() );
 
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_NAME, COLUME_NOTE_ID + " = ?",
-                new String[] { String.valueOf(note.getNoteId()) });
+        db.delete(TABLE_NAME, COLUME_ID + " = ?",
+                new String[] { String.valueOf(note.getId()) });
         db.close();
     }
 
